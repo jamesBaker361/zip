1fdfd50db22e95b5b6a6f4aa0f13d1c93c1bfe63
James Baker <jlbaker361@gmail.com>
2025-01-24 13:39:25 -0500

    added dot function

--- image_helpers.py
+++ image_helpers.py
@@ -6,12 +6,12 @@ def draw_line_on_image(src_image:Image.Image, left:list[int], right:list[int])->
     Given an image and two [u,v] coordinates, returns a black and white version of the image
     with a red line connecting the two coordinates
     Args
-        src_image (PIL.Image.Image)
+        src_image (Image.Image)
         left (list[int]) len==2 u,v coordinate of left point of line, 
         right (list[int]) len==2 u,v coordinate of right point of line, 
 
     Returns
-        final_image (PIL.Image.Image)
+        final_image (Image.Image)
     """
 
     final_image=src_image.convert("L").convert("RGB")
@@ -22,9 +22,26 @@ def draw_line_on_image(src_image:Image.Image, left:list[int], right:list[int])->
 
     return final_image
 
+def draw_points_on_image(src_image:Image.Image, dot_position_list:list[tuple],radius:int=2)->Image.Image:
+    """
+
+    Args:
+        src_image (Image.Image): _description_
+
+    Returns:
+        Image.Image: _description_
+    """
+    draw=ImageDraw.Draw(src_image)
+    red=(255,0,0)
+    for xy in dot_position_list:
+        draw.circle(xy,radius,fill=red)
+
+    return src_image
+
 if __name__=="__main__":
     src_image=Image.open("input_1/frame0001.jpg")
     left=[0,408]
+    center=[950, 350]
     right=[1919,286]
-    final_image=draw_line_on_image(src_image,left,right)
+    final_image=draw_points_on_image(src_image,[left,right,center])
     final_image.save("final.jpg")
